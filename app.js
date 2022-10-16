eb77d522801e58cceb697eb164976b73ab7ab92f
sharpman13 <sharpman2001@gmail.com>
2022-10-16 21:40:23 +0200

    refactor: add alt in HTML and JS according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="tasks-area__button tasks-area__button-delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "tasks-area__btn-img";
+    deleteButtonImg.alt = 'delete button';
     deleteButton.appendChild(deleteButtonImg);
 
 
